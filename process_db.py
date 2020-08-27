2e8cd4f2be7b9b9f8b57034d63ed6122b4e2d5b3
Anna <anna.pamela@gmail.com>
2020-08-26 21:40:17 -0400

    test

---
+++ process_db.py
@@ -0,0 +1,15 @@
+import pickle
+
+pw_dbs = ['kegg', 'reactome', 'xcell']
+
+for db_name in pw_dbs:
+    pw_db = pickle.load(open(f'/Users/anna/PycharmProjects/na_parallel/databases/{db_name}.pkl', 'rb'))
+
+    for pw, genes in pw_db.items():
+        if '/' in pw:
+            pw = '-'.join(pw.split('/'))
+        with open(f'{db_name}/{pw}.txt', 'w') as f:
+            f.write(pw)
+            f.write('\n')
+            f.write('\t'.join(genes))
+        print(f'finished: {db_name} - {pw}')
